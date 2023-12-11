504f04c2a47f0ea8fc3e38fd1d2f6689b47a6a1e
N-Berns <n.berns@protonmail.com>
2023-12-11 10:07:52 +0800

    Database update

--- instatxt-db.sql
+++ instatxt-db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2023 at 01:29 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Dec 11, 2023 at 03:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,14 +33,6 @@ CREATE TABLE `contacts` (
   `lName` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `contacts`
---
-
-INSERT INTO `contacts` (`number`, `fName`, `lName`) VALUES
-('639917909540', 'Kerby', 'Paderogao'),
-('639958751284', 'Neil', 'Bernado');
-
 -- --------------------------------------------------------
 
 --
@@ -55,16 +47,6 @@ CREATE TABLE `msg` (
   `msg` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `msg`
---
-
-INSERT INTO `msg` (`msgID`, `senderNum`, `receiverNum`, `datetime`, `msg`) VALUES
-(1, '639958751284', '639917909540', '2023-11-28 18:54:21', 'Sender Test'),
-(2, '639917909540', '639958751284', '2023-11-28 11:55:00', 'Receiver Test'),
-(3, '639958751284', '639156874806', '2023-11-28 12:20:57', 'Sender test 2'),
-(4, '639156874806', '639958751284', '2023-11-28 12:20:57', 'Receiver test 2');
-
 -- --------------------------------------------------------
 
 --
@@ -72,11 +54,19 @@ INSERT INTO `msg` (`msgID`, `senderNum`, `receiverNum`, `datetime`, `msg`) VALUE
 --
 
 CREATE TABLE `user` (
+  `username` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
   `number` varchar(12) NOT NULL,
   `fName` varchar(15) NOT NULL,
-  `lName` varchar(15) NOT NULL,
-  `tag` varchar(10) NOT NULL
+  `lName` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `number`, `fName`, `lName`) VALUES
+('testUser', '123123123', '639956132620', 'Test', 'User');
 
 --
 -- Indexes for dumped tables
@@ -108,7 +98,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `msg`
 --
 ALTER TABLE `msg`
-  MODIFY `msgID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `msgID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
